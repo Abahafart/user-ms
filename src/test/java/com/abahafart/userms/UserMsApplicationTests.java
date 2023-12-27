aaab30eb1eb0e18baee7c13e7cf398ff99c4a500
Alfredo Miranda <aflores@palo-it.com>
2023-12-26 19:32:31 -0600

    init commit

---
+++ src/test/java/com/abahafart/userms/UserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abahafart.userms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
